243376de698c0a04ed299602d0616299ba54321a
DysaniazzZ <William.fzy@foxmail.com>
2018-01-21 10:43:28 +0800

    Layout1: initial commit

---
+++ layout1/src/main/java/com/dysania/hencoderlayout1/MainActivity.java
@@ -0,0 +1,67 @@
+package com.dysania.hencoderlayout1;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.StringRes;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager pager;
+    List<PageModel> pageModels = new ArrayList<>();
+
+    {
+        pageModels.add(new PageModel(R.string.title_square_image_view, R.layout.sample_square_image_view, R.layout.practice_square_image_view));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pager = findViewById(R.id.pager);
+        pager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+
+            @Override
+            public Fragment getItem(int position) {
+                PageModel pageModel = pageModels.get(position);
+                return PageFragment.newInstance(pageModel.sampleLayoutRes, pageModel.practiceLayoutRes);
+            }
+
+            @Override
+            public int getCount() {
+                return pageModels.size();
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return getString(pageModels.get(position).titleRes);
+            }
+        });
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(pager);
+    }
+
+    private class PageModel {
+        @StringRes
+        int titleRes;
+        @LayoutRes
+        int sampleLayoutRes;
+        @LayoutRes
+        int practiceLayoutRes;
+
+        PageModel(@StringRes int titleRes, @LayoutRes int sampleLayoutRes, @LayoutRes int practiceLayoutRes) {
+            this.titleRes = titleRes;
+            this.sampleLayoutRes = sampleLayoutRes;
+            this.practiceLayoutRes = practiceLayoutRes;
+        }
+    }
+}
