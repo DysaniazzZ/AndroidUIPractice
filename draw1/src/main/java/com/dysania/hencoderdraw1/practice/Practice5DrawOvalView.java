e7e80824e850b3a5ae4785d0e2c828fd038fafbd
DysaniazzZ <William.fzy@foxmail.com>
2018-01-25 21:02:09 +0800

    Draw1: draw oval

--- draw1/src/main/java/com/dysania/hencoderdraw1/practice/Practice5DrawOvalView.java
+++ draw1/src/main/java/com/dysania/hencoderdraw1/practice/Practice5DrawOvalView.java
@@ -2,9 +2,14 @@ package com.dysania.hencoderdraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.dysania.hencoderdraw1.UIUtil;
 
 public class Practice5DrawOvalView extends View {
 
@@ -23,7 +28,19 @@ public class Practice5DrawOvalView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        // 使用 canvas.drawOval() 方法画椭圆
+        int width = getWidth();
+        int height = getHeight();
 
-//        练习内容：使用 canvas.drawOval() 方法画椭圆
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLACK);
+
+        canvas.translate(width / 2, height / 2);
+        //Call requires API level 21
+        //canvas.drawOval(UIUtil.dpToPx(-70), -UIUtil.dpToPx(35), UIUtil.dpToPx(70), UIUtil.dpToPx(35), paint);
+
+        RectF rectF = new RectF(UIUtil.dpToPx(-70), -UIUtil.dpToPx(35), UIUtil.dpToPx(70), UIUtil.dpToPx(35));
+        canvas.drawOval(rectF, paint);
     }
 }
