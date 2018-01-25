2ac03cdb0b10cdad8e64edf4be02daa263a2c29a
DysaniazzZ <William.fzy@foxmail.com>
2018-01-25 20:55:22 +0800

    Draw1: draw point

--- draw1/src/main/java/com/dysania/hencoderdraw1/practice/Practice4DrawPointView.java
+++ draw1/src/main/java/com/dysania/hencoderdraw1/practice/Practice4DrawPointView.java
@@ -2,9 +2,13 @@ package com.dysania.hencoderdraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.dysania.hencoderdraw1.UIUtil;
 
 public class Practice4DrawPointView extends View {
 
@@ -23,9 +27,20 @@ public class Practice4DrawPointView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        // 使用 canvas.drawPoint() 方法画点
+        // 一个圆点，一个方点
+        // 圆点和方点的切换使用 paint.setStrokeCap(cap)：`ROUND` 是圆点，`BUTT` 或 `SQUARE` 是方点
+        int width = getWidth();
+        int height = getHeight();
 
-//        练习内容：使用 canvas.drawPoint() 方法画点
-//        一个圆点，一个方点
-//        圆点和方点的切换使用 paint.setStrokeCap(cap)：`ROUND` 是圆点，`BUTT` 或 `SQUARE` 是方点
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(UIUtil.dpToPx(30));
+
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        canvas.drawPoint(width / 4, height / 2, paint);
+
+        paint.setStrokeCap(Paint.Cap.SQUARE);
+        canvas.drawPoint(width / 4 * 3, height / 2, paint);
     }
 }
