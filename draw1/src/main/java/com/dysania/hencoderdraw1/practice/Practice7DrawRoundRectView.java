5ff23c4ababe84dde2ef5ebb2634ec84dd7127b4
DysaniazzZ <William.fzy@foxmail.com>
2018-01-26 09:58:18 +0800

    Draw1: draw round rect

--- draw1/src/main/java/com/dysania/hencoderdraw1/practice/Practice7DrawRoundRectView.java
+++ draw1/src/main/java/com/dysania/hencoderdraw1/practice/Practice7DrawRoundRectView.java
@@ -2,9 +2,14 @@ package com.dysania.hencoderdraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.dysania.hencoderdraw1.UIUtil;
 
 public class Practice7DrawRoundRectView extends View {
 
@@ -23,7 +28,19 @@ public class Practice7DrawRoundRectView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        // 使用 canvas.drawRoundRect() 方法画圆角矩形
+        int width = getWidth();
+        int height = getHeight();
 
-//        练习内容：使用 canvas.drawRoundRect() 方法画圆角矩形
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLACK);
+
+        canvas.translate(width / 2, height / 2);
+        // Call requires API level 21
+        // canvas.drawRoundRect(UIUtil.dpToPx(-65), -UIUtil.dpToPx(35), UIUtil.dpToPx(65), UIUtil.dpToPx(35), UIUtil.dpToPx(15), UIUtil.dpToPx(15), paint);
+
+        RectF rectF = new RectF(UIUtil.dpToPx(-65), -UIUtil.dpToPx(35), UIUtil.dpToPx(65), UIUtil.dpToPx(35));
+        canvas.drawRoundRect(rectF, UIUtil.dpToPx(15), UIUtil.dpToPx(15), paint);
     }
 }
