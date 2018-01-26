4d2f96bf71bc0cde2a3cc9ffe69c84da86d9f947
DysaniazzZ <William.fzy@foxmail.com>
2018-01-26 10:30:04 +0800

    Draw1: draw path

--- draw1/src/main/java/com/dysania/hencoderdraw1/practice/Practice9DrawPathView.java
+++ draw1/src/main/java/com/dysania/hencoderdraw1/practice/Practice9DrawPathView.java
@@ -2,6 +2,10 @@ package com.dysania.hencoderdraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -23,7 +27,27 @@ public class Practice9DrawPathView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        // 使用 canvas.drawPath() 方法画心形
+        int width = getWidth();
+        int height = getHeight();
 
-//        练习内容：使用 canvas.drawPath() 方法画心形
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.RED);
+
+        canvas.translate(width / 2, height / 2);
+        Path path = new Path();
+
+        // Call requires API level 21
+        // path.addArc(-180, -180, 0, 0, -225, 225);
+        // path.arcTo(0, -180, 180, 0, -180, 225, false);
+        
+        RectF leftRectF = new RectF(-180, -180, 0, 0);
+        RectF rightRectF = new RectF(0, -180, 180, 0);
+        path.addArc(leftRectF, -225, 225);
+        path.arcTo(rightRectF, -180, 225, false);
+
+        path.lineTo(0, 140);
+        canvas.drawPath(path, paint);
     }
 }
