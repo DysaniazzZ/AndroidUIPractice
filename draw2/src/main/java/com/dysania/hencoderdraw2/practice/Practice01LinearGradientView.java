79b562b2a01fe5ba4812b4c220b7676a9304bad4
DysaniazzZ <William.fzy@foxmail.com>
2018-01-29 19:10:24 +0800

    Draw2: linear gradient shader

--- draw2/src/main/java/com/dysania/hencoderdraw2/practice/Practice01LinearGradientView.java
+++ draw2/src/main/java/com/dysania/hencoderdraw2/practice/Practice01LinearGradientView.java
@@ -2,7 +2,10 @@ package com.dysania.hencoderdraw2.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,12 +28,16 @@ public class Practice01LinearGradientView extends View {
     {
         // 用 Paint.setShader(shader) 设置一个 LinearGradient
         // LinearGradient 的参数：坐标：(100, 100) 到 (500, 500) ；颜色：#E91E63 到 #2196F3
+        // x0, y0, x1, y1：渐变的两个端点的位置 
+        // color0, color1：端点的颜色 
+        // tile：端点范围之外的着色规则，类型是 TileMode。TileMode 一共有 3 个值可选： CLAMP, MIRROR 和 REPEAT。
+        LinearGradient linearGradient = new LinearGradient(100, 100, 500, 500, Color.parseColor("#E91E63"), Color.parseColor("#2196F3"), Shader.TileMode.CLAMP);
+        paint.setShader(linearGradient);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
         canvas.drawCircle(300, 300, 200, paint);
     }
 }
