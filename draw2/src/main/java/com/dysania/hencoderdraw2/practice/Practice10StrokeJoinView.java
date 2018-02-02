01f75ae0b478f077df6154165464cbb38c79385b
DysaniazzZ <William.fzy@foxmail.com>
2018-02-02 20:09:59 +0800

    Draw2: stroke join

--- draw2/src/main/java/com/dysania/hencoderdraw2/practice/Practice10StrokeJoinView.java
+++ draw2/src/main/java/com/dysania/hencoderdraw2/practice/Practice10StrokeJoinView.java
@@ -35,21 +35,22 @@ public class Practice10StrokeJoinView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        canvas.save();
-
         // 使用 Paint.setStrokeJoin() 来设置不同的拐角形状
 
+        canvas.save();
         canvas.translate(100, 100);
         // 第一种形状：MITER
+        paint.setStrokeJoin(Paint.Join.MITER);
         canvas.drawPath(path, paint);
 
         canvas.translate(300, 0);
         // 第二种形状：BEVEL
+        paint.setStrokeJoin(Paint.Join.BEVEL);
         canvas.drawPath(path, paint);
 
         canvas.translate(300, 0);
         // 第三种形状：ROUND
+        paint.setStrokeJoin(Paint.Join.ROUND);
         canvas.drawPath(path, paint);
 
         canvas.restore();
