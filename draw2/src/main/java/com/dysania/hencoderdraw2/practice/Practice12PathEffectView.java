f738af3830303e62ef29a477fbb78294e830fd3c
DysaniazzZ <William.fzy@foxmail.com>
2018-02-03 17:34:42 +0800

    Draw2: path effect

--- draw2/src/main/java/com/dysania/hencoderdraw2/practice/Practice12PathEffectView.java
+++ draw2/src/main/java/com/dysania/hencoderdraw2/practice/Practice12PathEffectView.java
@@ -2,8 +2,14 @@ package com.dysania.hencoderdraw2.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.ComposePathEffect;
+import android.graphics.CornerPathEffect;
+import android.graphics.DashPathEffect;
+import android.graphics.DiscretePathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.SumPathEffect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,6 +32,7 @@ public class Practice12PathEffectView extends View {
 
     {
         paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(2);
 
         path.moveTo(50, 100);
         path.rLineTo(50, 100);
@@ -38,39 +45,59 @@ public class Practice12PathEffectView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
         // 使用 Paint.setPathEffect() 来设置不同的 PathEffect
 
         // 第一处：CornerPathEffect
+        // param1：圆角半径
+        CornerPathEffect cornerPathEffect = new CornerPathEffect(20);
+        paint.setPathEffect(cornerPathEffect);
         canvas.drawPath(path, paint);
 
         canvas.save();
         canvas.translate(500, 0);
         // 第二处：DiscretePathEffect
+        // param1：每个线段长度；param2：偏移量
+        DiscretePathEffect discretePathEffect = new DiscretePathEffect(20, 5);
+        paint.setPathEffect(discretePathEffect);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 200);
         // 第三处：DashPathEffect
+        // param1：虚线的格式，必须为偶数，画线长度、空白长度、画线长度、空白长度；param2：虚线偏移量
+        DashPathEffect dashPathEffect = new DashPathEffect(new float[]{20, 10, 5, 10}, 0);
+        paint.setPathEffect(dashPathEffect);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(500, 200);
         // 第四处：PathDashPathEffect
+        Path dashPath = new Path();
+        dashPath.lineTo(20, -30);
+        dashPath.lineTo(40, 0);
+        dashPath.close();
+        // param1：绘制的 path；param2：相邻 path 间的距离；param3：虚线的偏移量；param4：拐弯改变时的转换方式
+        PathDashPathEffect pathDashPathEffect = new PathDashPathEffect(dashPath, 50, 0, PathDashPathEffect.Style.MORPH);
+        paint.setPathEffect(pathDashPathEffect);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 400);
         // 第五处：SumPathEffect
+        SumPathEffect sumPathEffect = new SumPathEffect(discretePathEffect, dashPathEffect);
+        paint.setPathEffect(sumPathEffect);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(500, 400);
         // 第六处：ComposePathEffect
+        // param1：后应用的效果；param2：先应用的效果
+        ComposePathEffect composePathEffect = new ComposePathEffect(dashPathEffect, discretePathEffect);
+        paint.setPathEffect(composePathEffect);
         canvas.drawPath(path, paint);
         canvas.restore();
     }
