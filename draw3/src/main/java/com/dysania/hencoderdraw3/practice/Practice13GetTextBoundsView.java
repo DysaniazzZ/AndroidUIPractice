7e63df0289c546ebdd2cbb285d1e8927e8e5830e
DysaniazzZ <William.fzy@foxmail.com>
2018-02-22 14:48:57 +0800

    Draw3: get text bounds

--- draw3/src/main/java/com/dysania/hencoderdraw3/practice/Practice13GetTextBoundsView.java
+++ draw3/src/main/java/com/dysania/hencoderdraw3/practice/Practice13GetTextBoundsView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -11,12 +12,7 @@ import android.view.View;
 public class Practice13GetTextBoundsView extends View {
     Paint paint1 = new Paint(Paint.ANTI_ALIAS_FLAG);
     Paint paint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
-    String text1 = "A";
-    String text2 = "a";
-    String text3 = "J";
-    String text4 = "j";
-    String text5 = "Â";
-    String text6 = "â";
+    String[] texts = {"A", "a", "J", "j", "Â", "â"};
     int top = 200;
     int bottom = 400;
 
@@ -42,19 +38,19 @@ public class Practice13GetTextBoundsView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
         canvas.drawRect(50, top, getWidth() - 50, bottom, paint1);
-
         // 使用 Paint.getTextBounds() 计算出文字的显示区域
         // 然后计算出文字的绘制位置，从而让文字上下居中
         // 这种居中算法的优点是，可以让文字精准地居中，分毫不差
 
         int middle = (top + bottom) / 2;
-        canvas.drawText(text1, 100, middle, paint2);
-        canvas.drawText(text2, 200, middle, paint2);
-        canvas.drawText(text3, 300, middle, paint2);
-        canvas.drawText(text4, 400, middle, paint2);
-        canvas.drawText(text5, 500, middle, paint2);
-        canvas.drawText(text6, 600, middle, paint2);
+        Rect textBounds = new Rect();
+
+        for (int i = 0; i < texts.length; i++) {
+            paint2.getTextBounds(texts[i], 0, texts[i].length(), textBounds);
+            // top 的值为负（因为它在 baseline 的上方）；bottom 的值为正（因为它在 baseline 的下方）
+            int yOffset = -(textBounds.top + textBounds.bottom) / 2;
+            canvas.drawText(texts[i], 100 * (i + 1), middle + yOffset, paint2);
+        }
     }
 }
