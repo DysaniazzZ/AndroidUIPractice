c7e392fe8b7d48bf02f9cfd65b32eb6e4fc2eff4
DysaniazzZ <William.fzy@foxmail.com>
2018-02-22 12:01:20 +0800

    Draw3: draw text

--- draw3/src/main/java/com/dysania/hencoderdraw3/practice/Practice01DrawTextView.java
+++ draw3/src/main/java/com/dysania/hencoderdraw3/practice/Practice01DrawTextView.java
@@ -30,8 +30,7 @@ public class Practice01DrawTextView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
         // 使用 drawText() 来绘制文字
-        // 文字坐标： (50, 100)
+        canvas.drawText(text, 50, 100, paint);
     }
 }
